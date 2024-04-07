f9c10e2f6be8a0884b0e7d94133bf412077c43b0
peer <ai.under.the.stars@gmail.com>
2024-04-07 12:32:43 -0400

    Added player page, link to player page in footer

---
+++ YoutubeClient/Controllers/YoutubeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using YoutubeClient.Models;
+
+namespace YoutubeClient.Controllers
+{
+    public class YoutubeController : Controller
+    {
+
+        private readonly ILogger<YoutubeController> _logger;
+
+        public YoutubeController(ILogger<YoutubeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Player()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
